95dc3d4e5bddec76ec6e9f3a5b1ecfa004efd464
falvarez7 <falvarez7@mail.csuchico.edu>
2017-12-06 00:06:07 -0800

    Placeholder for .cpp file

---
+++ Background.cpp
@@ -0,0 +1,14 @@
+//Author: Frederick Alvarez
+
+/*
+Function Ideas
+.getPixel(int,int)
+-uses postions from for loop to access the background image pixel for replacement
+
+.RowSize()
+self explanitory
+.ColSize()
+selfexplanatory
+
+.getPixelMatrix
+*/
