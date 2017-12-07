d66c2641460d1799c2dc64c4461335d9890e1f00
falvarez7 <falvarez7@mail.csuchico.edu>
2017-12-07 01:54:55 -0800

    Function declarations and functions for Background class.

--- backGround.h
+++ backGround.h
@@ -1,4 +1,25 @@
 //Author: Frederick Alvarez
+#ifndef _BACKGROUND_H_
+#define _BACKGROUND_H_
 
+#include<string>
 #include"bitmap.h"
-//This is a placeholder file for the class Background
+
+using namespace std;
+
+class Background
+{
+    private:
+    Bitmap setting;
+    vector< vector<Pixel> > backgroundMatrix;
+    Pixel dot;
+    public:
+    Background();//constructor
+    Pixel getPixel(int,int);
+    int colSize();
+    int rowSize();
+    void setMatrix(std::string);
+    void setBitmap(std::string);
+};
+
+#endif
