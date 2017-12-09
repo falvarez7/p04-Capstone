b69d89d73bb42295491cb41add8656f4d4e54186
falvarez7 <falvarez7@mail.csuchico.edu>
2017-12-08 22:07:50 -0800

    Final Version

--- backGround.h
+++ backGround.h
@@ -14,11 +14,17 @@ class Background
     vector< vector<Pixel> > backgroundMatrix;
     Pixel dot;
     public:
-    Background();//constructor
+    //constructor
+    Background();
+    //Retrieves Pixel at position of parameters(row,col)
     Pixel getPixel(int,int);
+    //returns the amount of columns in a Background variable
     int colSize();
+    //returns the amount of rows in a Background variable
     int rowSize();
+    //sets the vector< vector<Pixel> > member variable
     void setMatrix(std::string);
+    //sets the Bitmap member variable 
     void setBitmap(std::string);
 };
 
