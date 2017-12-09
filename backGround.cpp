1567ef0236c4f1b5afc23b029feaebd3dd425c0b
falvarez7 <falvarez7@mail.csuchico.edu>
2017-12-08 22:07:29 -0800

    Final Version

--- backGround.cpp
+++ backGround.cpp
@@ -4,25 +4,28 @@
 #include<vector>
 #include"backGround.h"
 using namespace std;
-
+//Constructor
 Background::Background()
 {
+//The rest of the variables are purposefully left undefined.
 dot.red = 0;
 dot.blue = 0;
 dot.green = 0;
 }
 
+//function declarations
 void Background::setBitmap(std::string filename)
 {
     setting.open(filename);
 }
+
+
 void Background::setMatrix(std::string filename)
 {
     setting.open(filename);
-        backgroundMatrix = setting.toPixelMatrix();
+    backgroundMatrix = setting.toPixelMatrix();
 
 }
-
 
 
 Pixel Background::getPixel(int row, int col)
@@ -31,10 +34,13 @@ Pixel Background::getPixel(int row, int col)
 
 }
 
+
 int Background::rowSize()
 {
     return backgroundMatrix.size();
 }
+
+
 int Background::colSize()
 {
     return backgroundMatrix[0].size();
